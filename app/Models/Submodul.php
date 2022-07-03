d6bba0853d2de2f6b37dccc2b135dfe0171c3840
ilhamsg7 <sinatrio20@gmail.com>
2022-07-03 09:05:05 +0700

    Model & Migration

--- app/Models/Submodul.php
+++ app/Models/Submodul.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model {
+class Submodul extends Model {
     use HasFactory;
-    protected $fillable = ['name'];
+
+    protected $table = 'submoduls';
+    protected $guarded = ['id'];
+    protected $with = ['modul'];
 
     public function modul() {
-        return $this->hasMany(Modul::class);
+        return $this->belongsTo(Modul::class);
     }
 }
